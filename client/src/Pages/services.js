ccdef7a80d156f012854bd7a1d9eb1d6d4df0842
David N <nguyen.david94579@gmail.com>
2021-02-25 13:43:16 -0800

    Changes to services page including picking services menu change.

--- client/src/Pages/services.js
+++ client/src/Pages/services.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {useQuery} from "@apollo/client";
 import gql from 'graphql-tag'
-import { Item, Menu} from "semantic-ui-react";
+import {Item, Menu} from "semantic-ui-react";
 
 import ServiceItem from "../Components/ServiceItem";
 import Banner from "../Components/Banner";
@@ -26,38 +26,39 @@ function Services() {
         <div>
             <Banner/>
             <div className="form-container ">
-                <Menu secondary className='menu-center'>
-                    <Menu.Item
-                        name='manicures'
-                        active={activeItem === 'manicures'}
-                        onClick={handleItemClick}
-                    />
-                    <Menu.Item
-                        name='pedicures'
-                        active={activeItem === 'pedicures'}
-                        onClick={handleItemClick}
-                    />
-                    <Menu.Item
-                        name='waxings'
-                        active={activeItem === 'waxings'}
-                        onClick={handleItemClick}
-                    />
-                </Menu>
                 <div className='menu-center'>
-                    {
-                        loading ? (<h1>Loading...</h1>) : (
-                            services &&
-                            services.filter(service => (
-                                service.category === activeItem
-                            )).map(service => (
-                                <Item.Group key={service.id}>
-                                    <ServiceItem service={service} activeItem={activeItem}/>
-                                </Item.Group>
-                            ))
-                        )
-                    }
+                    <Menu secondary pointing compact>
+                        <Menu.Item
+                            name='manicures'
+                            active={activeItem === 'manicures'}
+                            onClick={handleItemClick}
+                        />
+                        <Menu.Item
+                            name='pedicures'
+                            active={activeItem === 'pedicures'}
+                            onClick={handleItemClick}
+                        />
+                        <Menu.Item
+                            name='waxings'
+                            active={activeItem === 'waxings'}
+                            onClick={handleItemClick}
+                        />
+                    </Menu>
                 </div>
+                {
+                    loading ? (<h1>Loading...</h1>) : (
+                        services &&
+                        services.filter(service => (
+                            service.category === activeItem
+                        )).map(service => (
+                            <Item.Group key={service.id}>
+                                <ServiceItem className='item-center' service={service} activeItem={activeItem}/>
+                            </Item.Group>
+                        ))
+                    )
+                }
             </div>
+
         </div>
 
     )
